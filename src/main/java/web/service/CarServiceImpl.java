2605104ee7b2450890e5e33d3c2ccf5ffbc111d9
bitmaxik <bitmaxik@gmail.com>
2023-08-28 18:13:01 +0300

    add change PP2.2.2

--- src/main/java/web/service/CarServiceImpl.java
+++ src/main/java/web/service/CarServiceImpl.java
@@ -1,12 +1,12 @@
 package web.service;
 
-import org.springframework.stereotype.Component;
+import org.springframework.stereotype.Service;
 import web.model.Car;
 
 import java.util.List;
 import java.util.stream.Collectors;
 
-@Component
+@Service
 public class CarServiceImpl implements CarService {
 
     @Override
